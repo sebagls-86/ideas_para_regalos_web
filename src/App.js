a96b3d9ec0376f303e558060805f03750685ddc7
Marcos <marcos.kukuchka@gmail.com>
2023-01-04 23:07:17 -0300

    agregando css de colores fuentes y botones

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 function App() {
   return (
-    <Container fluid>
+    <Container fluid >
+    <h1>Test colores</h1>
       <Row>
-        <Col className="bg-danger p-5 m-5 text-light text-center">Ideas Para Regalos</Col>
+        <Col className="p-5 m-5 text-light text-center">
+        <a href="#" className="btn btn-primary-dark px-5">Boton 1</a>
+        </Col>
+        <Col className=" p-5 m-5 text-light text-center">
+        <a href="#" className="btn btn-primary-dark px-5">Boton 2 con </a>
+        </Col>
+        <Col className=" p-5 m-5 text-light text-center">
+        <a href="#" className="btn btn-outline-dark px-5">Boton 2 con </a>
+        </Col>
       </Row>
     </Container>
   );
