a048a0ea1217f48940c717b39a8e1635d06d68ca
Marcos <marcos.kukuchka@gmail.com>
2023-01-10 19:44:12 -0300

    Se agregó react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+
+import HomePage from './pages/homePage/HomePage';
+import NotFoundPage from './pages/notFoundPage/NotFound';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage/>} />
+      <Route path="*" element={<NotFoundPage/>} />
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
