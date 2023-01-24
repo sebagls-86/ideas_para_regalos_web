d9e38260d97120ad835aaef5ea75c8b8497afa60
Marcos Kukuchka <marcos.kukuchka@gmail.com>
2023-01-23 21:30:35 -0300

    Cambios colores

--- src/modules/login/Login.jsx
+++ src/modules/login/Login.jsx
@@ -5,8 +5,8 @@ function Login() {
     <>
       <h2 className="fz-15 fw-600 mt-2">Ingresá a tu cuenta</h2>
       <div className="d-flex flex-column flex-xl-row gap-2 mt-3">
-        <button className="btn primary__button">Registrarse</button>
         <button className="btn secondary__button">Registrarse</button>
+        <button className="btn primary__button">Iniciar sesión</button>
       </div>
     </>
   );
