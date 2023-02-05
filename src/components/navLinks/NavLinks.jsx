a569aa2e9a3f045cc77b3f8c96aa68f25add4ce5
Marcos Kukuchka <marcos.kukuchka@gmail.com>
2023-02-04 23:21:49 -0300

    se modifico el layout css

--- src/components/navLinks/NavLinks.jsx
+++ src/components/navLinks/NavLinks.jsx
@@ -1,15 +1,14 @@
 import React from "react";
-import styles from "./css/navLinks.module.css";
+import styles from "./navLinks.module.css";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
-function NavLinks({ title, icon, url }) {
+function NavLinks({icon, url }) {
   return (
     <li className="pt-0 pt-md-3">
       <Link to={url}>
         <div className={styles.link}>
           {icon} 
-          <p className={styles.title}>{title}</p>
         </div>
       </Link>
     </li>
--- src/modules/nav/Nav.jsx
+++ src/modules/nav/Nav.jsx
@@ -13,31 +13,29 @@ function Nav() {
       </Link>
       <ul className={styles.nav__ul}>
         <NavLinks
-          title="Inicio"
           url="/"
-          icon={<AiOutlineHome className="mr-3 fw-700 fs-3" />}
+          icon={<AiOutlineHome className="fw-700 fs-3" />}
         />
         <NavLinks
-          title="Explorar"
           url="/explorar"
-          icon={<AiOutlineCompass className="mr-3 fw-700 fs-3" />}
+          icon={<AiOutlineCompass className="fw-700 fs-3" />}
         />
         <NavLinks
-          title="Notificaciones"
           url="/notificaciones"
-          icon={<AiOutlineBell className="mr-3 fw-700 fs-3" />}
+          icon={<AiOutlineBell className=" fw-700 fs-3" />}
         />
         <NavLinks
-          title="Eventos"
           url="/eventos"
-          icon={<AiOutlineCalendar className="mr-3 fw-700 fs-3" />}
+          icon={<AiOutlineCalendar className=" fw-700 fs-3" />}
         />
          <NavLinks
-          title="Perfil"
           url="/perfil"
-          icon={<AiOutlineUser className="mr-3 fw-700 fs-3" />}
+          icon={<AiOutlineUser className="fw-700 fs-3" />}
         />
       </ul>
+      <Link to="/" className={styles.nav__logo}>
+        <img src={logoimg} alt="Logo ideas para regalos" />
+      </Link>
     </nav>
   );
 }
