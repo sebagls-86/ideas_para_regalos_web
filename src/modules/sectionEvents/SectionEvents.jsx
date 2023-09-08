6e803eb2d1fcac0f952c1049b511313d0f73e70d
anastasiakim <bkim.anastasia@gmail.com>
2023-09-08 18:57:59 +0900

    changed categorias and eventos, testing products

--- src/modules/sectionEvents/SectionEvents.jsx
+++ src/modules/sectionEvents/SectionEvents.jsx
@@ -1,26 +1,78 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import valentin from "../../assets/san-valentin-event.png";
+import christmas from "../../assets/xmas-event.png";
+import birthday from "../../assets/birthday-event.png";
 import styles from "./SectionEvents.module.css";
-import CardExplorer from "../../components/cardExplorer/CardExplorer";
+import { Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/autoplay";
+import { Link } from "react-router-dom";
 
-function SectionEvents({data, title}) {
-  
+
+export default function SectionEvents() {
   return (
-    <div>
-      <h2 className={styles.title}>{title}</h2>
-      <div className="d-flex flex-wrap gap-3 justify-content-center pt-3">
-        {data.map((category, index) => { 
-          return (
-            <CardExplorer
-              key={category.id}
-              label={category.name}
-              img={category.img}
-              url={`/explorar/${category.name.toLowerCase()}`}
-            />
-          );
-        })}
+    <>
+    <div className={styles.eventsContainer}>
+      <h2 className={styles.title}>Eventos</h2>
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        autoplay={{ delay: 3000 }}
+        breakpoints={{
+         
+          768: {
+            slidesPerView: 3,
+          },
+          
+          0: {
+            slidesPerView: 1,
+          },
+        }}
+        
+      >
+        <Link to="">
+        <SwiperSlide>
+          {" "}
+          <img
+            src={christmas}
+            alt="banner"
+            className={styles.eventImage}
+          />
+          <h3 id="category">Navidad</h3>
+        </SwiperSlide>
+        </Link>
+        <SwiperSlide className={styles.category}> 
+          <img
+            src={birthday}
+            alt="beauty"
+            className={styles.eventImage}
+          />
+          <h3>Cumpleaños</h3>
+          </SwiperSlide>
+        <SwiperSlide> <img
+            src={valentin}
+            alt="fashion"
+            className={styles.eventImage}
+          />
+          <h3>San Valentín</h3></SwiperSlide>
+        <SwiperSlide> <img
+            src={birthday}
+            alt="fashion"
+          />
+          <label>Belleza</label></SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
       </div>
-    </div>
+    </>
   );
 }
-
-export default SectionEvents;
--- src/modules/sectionFeatured/SectionFeatured.jsx
+++ src/modules/sectionFeatured/SectionFeatured.jsx
@@ -1,24 +1,80 @@
-import React from "react";
-import ProductCard from "../../components/productCard/ProductCard"
-import styles from "./sectionFeatured.module.css"
 
-function SectionFeatured({ data, title }) {
+import React, { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import valentin from "../../assets/san-valentin-event.png";
+import christmas from "../../assets/xmas-event.png";
+import birthday from "../../assets/birthday-event.png";
+import styles from "./sectionFeatured.module.css";
+import { Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/autoplay";
+import { Link } from "react-router-dom";
+
+
+export default function SectionFeatured() {
   return (
-    <div>
-      <h2 className={styles.title}>{title}</h2>
-      <div className="d-flex flex-wrap gap-3 justify-content-around pt-3">
-        {data.map((featured, index) => {
-          return (
-            <ProductCard
-              key={featured.id}
-              title={featured.name}
-              img={featured.img}
-            />
-          );
-        })}
+    <>
+    <div className={styles.productsContainer}>
+      <h2 className={styles.title}>Productos destacados</h2>
+      <Swiper
+
+        slidesPerView={4}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        autoplay={{ delay: 3000 }}
+        breakpoints={{
+         
+          768: {
+            slidesPerView: 3,
+          },
+          
+          0: {
+            slidesPerView: 1,
+          },
+        }}
+        
+      >
+        <Link to="">
+        <SwiperSlide>
+          {" "}
+          <img
+            src={christmas}
+            alt="banner"
+            className={styles.eventImage}
+          />
+          <h3 id="category">Navidad</h3>
+        </SwiperSlide>
+        </Link>
+        <SwiperSlide className={styles.category}> 
+          <img
+            src={birthday}
+            alt="beauty"
+            className={styles.eventImage}
+          />
+          <h3>Cumpleaños</h3>
+          </SwiperSlide>
+        <SwiperSlide> <img
+            src={valentin}
+            alt="fashion"
+            className={styles.eventImage}
+          />
+          <h3>San Valentín</h3></SwiperSlide>
+        <SwiperSlide> <img
+            src={birthday}
+            alt="fashion"
+          />
+          <label>Belleza</label></SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
       </div>
-    </div>
+    </>
   );
 }
-
-export default SectionFeatured;
