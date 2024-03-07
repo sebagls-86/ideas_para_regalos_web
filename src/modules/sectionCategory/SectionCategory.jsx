9d0d7ab92f4d62ef4e0562abaecfd20d9d0fc494
Seba <63512521+sebagls-86@users.noreply.github.com>
2024-03-07 07:15:39 -0300

    usuarios y sugerencias (#35)

--- src/modules/sectionCategory/SectionCategory.jsx
+++ src/modules/sectionCategory/SectionCategory.jsx
@@ -1,78 +1,74 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import electronics from "../../assets/simpleCard.png";
-import beauty from "../../assets/beauty-categoty.png";
-import fashion from "../../assets/fashion-category.png";
 import styles from "./SectionCategory.module.css";
-import { Pagination } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/autoplay";
 import { Link } from "react-router-dom";
 
-
 export default function SectionCategory() {
+  const [categories, setCategories] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+  
+  const fetchCategories = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/api/v1/categories");
+      if (response.ok) {
+        const responseData = await response.json();
+        const data = responseData.data || [];
+        setCategories(data);
+      } else {
+        console.error("Error fetching categories:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error fetching categories:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <>
     <div className={styles.categoryContainer}>
-      <h2 className={styles.title}>Categorías</h2>
+    <h2 className={styles.title}>Categorías</h2>
+    {loading ? (
+      <p>Loading...</p> // Indicador de carga
+    ) : (
       <Swiper
         slidesPerView={3}
         spaceBetween={30}
         pagination={{
           clickable: true,
         }}
-        modules={[Pagination]}
-        autoplay={{ delay: 3000 }}
         breakpoints={{
-         
           768: {
             slidesPerView: 3,
           },
-          
           0: {
             slidesPerView: 1,
           },
         }}
-        
+        loop={true}
+        autoplay={{ delay: 3000 }}
       >
-        <Link to="">
-        <SwiperSlide>
-          {" "}
-          <img
-            src={electronics}
-            alt="banner"
-            className={styles.categoryImage}
-          />
-          <h3 id="category">Electrónicos</h3>
-        </SwiperSlide>
-        </Link>
-        <SwiperSlide className={styles.category}> 
-          <img
-            src={beauty}
-            alt="beauty"
-            className={styles.categoryImage}
-          />
-          <h3>Belleza</h3>
-          </SwiperSlide>
-        <SwiperSlide> <img
-            src={fashion}
-            alt="fashion"
-            className={styles.categoryImage}
-          />
-          <h3>Moda</h3></SwiperSlide>
-        <SwiperSlide> <img
-            src={fashion}
-            alt="fashion"
-          />
-          <label>Belleza</label></SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-      </Swiper>
-      </div>
-    </>
+        {categories.map((category, index) => (
+            <Link to="" key={index}>
+              <SwiperSlide>
+                {category.image ? (
+                  <img
+                    src={`http://localhost:8080${category.image}`} // Agregar la URL base a la ruta de la imagen
+                    alt="banner"
+                    className={styles.categoryImage}
+                  />
+                ) : (
+                  <div className={styles.placeholder}></div> // Placeholder en caso de que no haya imagen
+                )}
+                <h3>{category.name}</h3>
+              </SwiperSlide>
+            </Link>
+          ))}
+        </Swiper>
+      )}
+    </div>
   );
 }
