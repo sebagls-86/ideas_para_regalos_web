2a84f034e9a312d63586bd9bf896fe0ebfe4d2cd
sebagls-86 <sebastianglopezsosa@gmail.com>
2024-04-08 18:19:04 -0300

    auth0

--- src/index.js
+++ src/index.js
@@ -9,12 +9,14 @@ import App from './App';
  const dom = "dev-oraf1nl35nag2oxd.us.auth0.com";
  const clientID = "WTy5RLUirpCMGFljN9YsxVOvzCF6XT4m";
  const aud = "https://dev-oraf1nl35nag2oxd.us.auth0.com/api/v2/";
+ const redirectUri = process.env.NODE_ENV === 'production' ? 'https://ideas-para-regalos-web.vercel.app' : 'http://localhost:3000';
+
 
 root.render(
 <Auth0Provider
             domain={dom}
             clientId={clientID}
-            redirectUri={window.location.origin}
+            redirectUri={redirectUri}
             audience={aud}   
             scope= "read:current_user update:current_user_metadata"
   >
