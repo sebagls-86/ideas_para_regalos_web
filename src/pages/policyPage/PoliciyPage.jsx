00b353c70652f9c4de0ecdf8cd13ec33a8acb473
anastasiakim <bkim.anastasia@gmail.com>
2024-06-10 14:31:22 +0900

    better wording

--- src/pages/policyPage/PoliciyPage.jsx
+++ src/pages/policyPage/PoliciyPage.jsx
@@ -16,33 +16,33 @@ function PolicyPage() {
         </h5>
 
         <p className={styles.description}>
-          Nos comprometemos a proteger tu privacidad. Estas Políticas de
+          Nos comprometemos a proteger su privacidad. Estas Políticas de
           Privacidad explican cómo recopilamos, usamos, divulgamos y protegemos
-          tu información personal cuando visitas nuestra página web.
+          su información personal cuando visita nuestra página web.
         </p>
 
         <h4 className={styles.titulo_}>1. Información que Recopilamos</h4>
         <ul className="mb-2">
-          Recopilamos los siguientes datos personales para mejorar tu
+          Recopilamos los siguientes datos personales para mejorar su
           experiencia en nuestro sitio:
           <li className={styles.lista}>
-            Nombre: Para personalizar nuestros servicios y dirigirnos a ti de
+            Nombre: Para personalizar nuestros servicios y dirigirnos a usted de
             manera más adecuada.
           </li>
           <li className={styles.lista}>
-            Correo electrónico: Para enviarte actualizaciones, ofertas y
-            comunicaciones importantes relacionadas con tu cuenta.
+            Correo electrónico: Para enviar actualizaciones, ofertas y
+            comunicaciones importantes relacionadas con su cuenta.
           </li>
           <li className={styles.lista}>
-            Foto de perfil: Para que puedas personalizar tu cuenta y facilitar
-            tu identificación en la plataforma.
+            Foto de perfil: Para que puedas personalizar su cuenta y facilitar
+            su identificación en la plataforma.
           </li>
         </ul>
 
         <h4 className={styles.titulo_}>2. Métodos de Recopilación</h4>
         <p className="mb-2">
           La información se recopila a través del registro de usuario en nuestro
-          sitio web. Al registrarte, nos proporcionás voluntariamente esta
+          sitio web. Al registrarse, nos proporciona voluntariamente esta
           información para poder acceder y utilizar nuestros servicios.
         </p>
 
@@ -52,11 +52,11 @@ function PolicyPage() {
         <p className="mb-2">
           La información que recopilamos tiene diversos propósitos, entre ellos
           la personalización del servicio. Utilizamos estos datos para mejorar y
-          adaptar nuestros servicios de acuerdo con tus preferencias
+          adaptar nuestros servicios de acuerdo con sus preferencias
           individuales y necesidades específicas. Esta personalización nos
-          permite ofrecerte una experiencia más relevante y satisfactoria,
-          asegurándonos de que recibas contenido, recomendaciones y
-          funcionalidades que sean útiles y pertinentes para vos.
+          permite ofrecerle una experiencia más relevante y satisfactoria,
+          asegurándonos de que reciba contenido, recomendaciones y
+          funcionalidades que sean útiles y pertinentes para usted.
         </p>
 
         <h4 className={styles.titulo_}>
@@ -64,25 +64,25 @@ function PolicyPage() {
         </h4>
 
         <p className="mb-2">
-          Nos comprometemos a no compartir tu información personal con terceros
+          Nos comprometemos a no compartir su información personal con terceros
           bajo ninguna circunstancia, salvo que sea requerido por ley.
         </p>
 
         <h4 className={styles.titulo_}>5. Derechos de los Usuarios</h4>
         <ul className="mb-2">
-          Tenés derecho a todos los derechos previstos en la ley Argentina, que
+          Tiene derecho a todos los derechos previstos en la ley Argentina, que
           incluyen:
           <li className={styles.lista}>
-            Derecho de acceso: Podés solicitar información sobre los datos
-            personales que tenemos sobre vos.
+            Derecho de acceso: Puede solicitar información sobre los datos
+            personales que tenemos sobre usted.
           </li>
           <li className={styles.lista}>
-            Derecho de rectificación: Tenés la facultad de solicitar la
+            Derecho de rectificación: Tiene la facultad de solicitar la
             corrección de cualquier dato inexacto o incompleto.
           </li>
           <li className={styles.lista}>
-            Derecho de eliminación: Tenés la posibilidad de solicitar la
-            eliminación de tus datos personales.
+            Derecho de eliminación: Tiene la posibilidad de solicitar la
+            eliminación de sus datos personales.
           </li>
         </ul>
 
@@ -91,7 +91,7 @@ function PolicyPage() {
           La información personal se almacena en bases de datos protegidas con
           contraseñas. Solo los administradores de la página tienen acceso a
           estas bases de datos, garantizando la seguridad y confidencialidad de
-          tu información.
+          su información.
         </p>
 
         <h4 className={styles.titulo_}>7. Política de Cookies</h4>
@@ -101,7 +101,7 @@ function PolicyPage() {
 
         <h4 className={styles.titulo_}>8. Enlaces a Otros Sitios</h4>
         <p className="mb-2">
-          No proporcionamos enlaces a otros sitios web. Tu navegación se limita
+          No proporcionamos enlaces a otros sitios web. Su navegación se limita
           exclusivamente a nuestro sitio.
         </p>
 
@@ -111,7 +111,7 @@ function PolicyPage() {
         <p className="mb-2">
           Revisamos y actualizamos nuestras políticas de privacidad cada seis
           meses para asegurarnos de que cumplen con las normativas vigentes y
-          proteger mejor tu información.
+          proteger mejor su información.
         </p>
 
         <h4 className={styles.titulo_}>10. Restricciones de Uso</h4>
@@ -122,14 +122,14 @@ function PolicyPage() {
 
         <h4 className={styles.titulo_}>11. Contacto</h4>
         <p className="mb-2">
-          Si tenés alguna pregunta sobre nuestras políticas de privacidad o
+          Si tiene alguna pregunta sobre nuestras políticas de privacidad o
           desea ejercer alguno de sus derechos, por favor contáctenos en: Ideas
           para Regalos Correo electrónico: ideaspararegalos@gmail.com
         </p>
 
         <p className="mb-4">
-          Agradecemos tu confianza y aseguramos que tu privacidad es de
-          máxima importancia para nosotros.
+          Agradecemos su confianza y aseguramos que su privacidad es de máxima
+          importancia para nosotros.
         </p>
       </div>
     </>
--- src/pages/termsPage/TermsPage.jsx
+++ src/pages/termsPage/TermsPage.jsx
@@ -16,19 +16,19 @@ function TermsPage() {
           Fecha de última actualización: 8 de junio de 2024.
         </h5>
         <p className={styles.description}>
-          Bienvenido a Ideas para Regalos. Al acceder a tu cuenta, te
-          comprometés a seguir nuestras políticas y normas, que están destinadas
-          a proteger tanto tus derechos como los de otros usuarios. Te
-          recomendamos que leas detenidamente nuestros Términos y Condiciones
-          para que estés plenamente informado sobre tus obligaciones y derechos
-          al utilizar nuestra plataforma.
+          Bienvenido a Ideas para Regalos. Al acceder a su cuenta, usted se
+          compromete a seguir nuestras políticas y normas, que están destinadas
+          a proteger tanto tus derechos como los de otros usuarios. Recomendamos
+          que lea detenidamente nuestros Términos y Condiciones para que esté
+          plenamente informado sobre sus obligaciones y derechos al utilizar
+          nuestra plataforma.
         </p>
-       
+
         <div className={styles.titulo_}>1. Aceptación de los Términos</div>
         <p className="mb-2">
-          Al acceder a nuestro sitio web, aceptás estar sujeto a estos Términos
-          de Uso y a todas las leyes y regulaciones aplicables. Si no estás de
-          acuerdo con alguno de estos términos, tenés prohibido usar o acceder a
+          Al acceder a nuestro sitio web, acepta estar sujeto a estos Términos
+          de Uso y a todas las leyes y regulaciones aplicables. Si no está de
+          acuerdo con alguno de estos términos, tiene prohibido usar o acceder a
           este sitio.
         </p>
         <div className={styles.titulo_}>2. Modificaciones de los Términos</div>
@@ -43,27 +43,27 @@ function TermsPage() {
         <div className={styles.titulo_}>3. Registro de Usuario</div>
         <p className="mb-2">
           Para utilizar ciertos servicios de nuestro sitio web, es posible que
-          debas registrarte y crear una cuenta. Te comprometés a proporcionar
+          deba registrarse y crear una cuenta. Se compromete a proporcionar
           información precisa y completa durante el proceso de registro y a
           actualizar dicha información según sea necesario.
         </p>
         <div className={styles.titulo_}>4. Privacidad</div>
         <p className="mb-2">
-          El uso de tu información personal está regulado por nuestra
+          El uso de su información personal está regulado por nuestra
           <Link to="" className={styles.link}>
             Política de Privacidad
           </Link>
-          . Esta política describe cómo recopilamos, utilizamos y protegemos tus
-          datos. Al utilizar nuestro sitio web, aceptás los términos y
+          . Esta política describe cómo recopilamos, utilizamos y protegemos sus
+          datos. Al utilizar nuestro sitio web, acepta los términos y
           condiciones establecidos, que tiene como objetivo garantizar la
-          seguridad y confidencialidad de tu información. Te recomendamos leerla
+          seguridad y confidencialidad de su información. Le recomendamos leerla
           detenidamente para comprender cómo manejamos tus datos y cuáles son
-          tus derechos en relación a la protección de la privacidad.
+          sus derechos en relación a la protección de la privacidad.
         </p>
         <div className={styles.titulo_}>5. Uso Aceptable</div>
         <p className="mb-2">
-         Te comprometés a utilizar nuestro sitio web de manera legal y
-          respetuosa. No podrás utilizar el sitio web para: Publicar o transmitir
+          Se compromete a utilizar nuestro sitio web de manera legal y
+          respetuosa. No podrá utilizar el sitio web para: Publicar o transmitir
           contenido ilegal, ofensivo, difamatorio o que infrinja derechos de
           terceros. Hacer uso no autorizado de los sistemas o redes de nuestro
           sitio web. Realizar actividades que puedan interferir con el
@@ -91,16 +91,16 @@ function TermsPage() {
         </p>
         <div className={styles.titulo_}>8. Indemnización</div>
         <p className="mb-2">
-          Aceptás indemnizar y mantener indemne a Ideas para Regalos y
-          a sus afiliados, empleados, agentes, socios y licenciantes, de
-          cualquier reclamo o demanda, incluyendo honorarios razonables de
-          abogados, realizados por cualquier tercero debido a o como resultado
-          de su violación de estos Términos de Uso o su violación de cualquier
-          ley o derecho de un tercero.
+          Acepta indemnizar y mantener indemne a Ideas para Regalos y a sus
+          afiliados, empleados, agentes, socios y licenciantes, de cualquier
+          reclamo o demanda, incluyendo honorarios razonables de abogados,
+          realizados por cualquier tercero debido a o como resultado de su
+          violación de estos Términos de Uso o su violación de cualquier ley o
+          derecho de un tercero.
         </p>
         <div className={styles.titulo_}>9. Terminación</div>
         <p className="mb-2">
-          Nos reservamos el derecho de terminar o suspender tu cuenta y acceso
+          Nos reservamos el derecho de terminar o suspender su cuenta y acceso
           al sitio web en cualquier momento, sin previo aviso, por cualquier
           razón, incluyendo, pero no limitándose a, la violación de estos
           Términos de Uso.
@@ -113,10 +113,10 @@ function TermsPage() {
         </p>
         <div className={styles.titulo_}>11. Contacto</div>
         <p className="mb-2">
-          Si tenés alguna pregunta sobre estos Términos de Uso, por favor
-          contactanos en: Ideas para Regalos
-          <br/>Correo electrónico:
-          ideaspararegalos@gmail.com
+          Si tiene alguna pregunta sobre estos Términos de Uso, por favor
+          contáctenos en: Ideas para Regalos
+          <br />
+          Correo electrónico: ideaspararegalos@gmail.com
         </p>
         <p className="mb-4">
           Si no estás de acuerdo con estos términos, por favor, no utilices
@@ -129,3 +129,4 @@ function TermsPage() {
 }
 
 export default TermsPage;
+
